c445c00af44e239ea63cb7bad7f81826ceea3107
RMN0605 <20c1020027dr@edu.tech.ac.jp>
2022-10-07 22:14:24 +0900

    Revert "2022年前期　ゲームプロジェクト完成"
    This reverts commit 48df81aefe5c8baef806e47efe1309c7d9faa473.

--- Assets/User/RumiRumi/Common/StageSetting.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StageSetting : MonoBehaviour
-{
-    public List<int> hantenNum = new List<int>(); //ステージごとの反転数
-}
